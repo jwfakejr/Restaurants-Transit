8e65b88a52e3bbb845643d93d21f8840afc1835c
Malek Trabelsi <malek.isims88@gmail.com>
2017-02-10 12:11:09 +0100

    Initial Commit

---
+++ Restaurants Transit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restaurants Transit
+//
+//  Created by Malek Trabelsi on 2/10/17.
+//  Copyright © 2017 Malek Trabelsi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
